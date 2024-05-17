c347973f943b0d80c082ef9f4f28a4259619f0a8
JackFilho <jksrodriguesfilho@gmail.com>
2024-05-17 17:31:30 -0300

    Alterando tela home

--- src/components/pages/bemvindo/index.js
+++ src/components/pages/bemvindo/index.js
@@ -1,34 +1,51 @@
-import React from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity, ImageBackground } from 'react-native';
-import * as Animatable from 'react-native-animatable';
-import { useNavigation } from '@react-navigation/native'
+import React from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    Image,
+    TouchableOpacity,
+    ImageBackground,
+} from "react-native";
+import * as Animatable from "react-native-animatable";
+import { useNavigation } from "@react-navigation/native";
 
 export default function BemVindo() {
     const navigation = useNavigation();
 
     return (
         <View style={styles.container}>
-                <View style={styles.containerLogo}>
+            <View style={styles.containerLogo}>
                 <Animatable.Image
                     animation="flipInY"
-                    source={require('../../../../assets/images/logo.png')}
-                    style={{ width: '100%' }}
+                    source={require("../../../../assets/images/logo.png")}
+                    style={{ width: "100%" }}
                     resizeMode="contain"
                 />
             </View>
-            <Animatable.View delay={600} animation="fadeInUp" style={styles.containerForm}>
-                <Text style={styles.title}>Venha conhecer a melhor pizzaria dos macacos de caruaru</Text>
-                <Text style={styles.text}>Faça seu cadastro e comece a fazer os pedidos</Text>
+            <Animatable.View
+                delay={600}
+                animation="fadeInUp"
+                style={styles.containerForm}
+            >
+                <Text style={styles.title}>
+                    Venha conhecer a melhor pizzaria dos macacos de caruaru
+                </Text>
+                <Text style={styles.text}>
+                    Faça seu cadastro e comece a fazer os pedidos
+                </Text>
                 <View style={styles.buttonContainer}>
                     <TouchableOpacity
                         style={styles.button}
-                        onPress={() => navigation.navigate('Cadastro')}>
+                        onPress={() => navigation.navigate("Cadastro")}
+                    >
                         <Text style={styles.buttonText}>Fazer cadastro</Text>
                     </TouchableOpacity>
 
                     <TouchableOpacity
                         style={styles.buttonlogin}
-                        onPress={() => navigation.navigate('Login')}>
+                        onPress={() => navigation.navigate("Login")}
+                    >
                         <Text style={styles.login}>Já possuo uma conta</Text>
                     </TouchableOpacity>
                 </View>
@@ -40,61 +57,59 @@ export default function BemVindo() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#6d351a"
+        backgroundColor: "#6d351a",
     },
     containerLogo: {
         flex: 2,
-        backgroundColor: '#6d351a',
-        justifyContent: 'center',
-        alignItems: 'center'
-
+        backgroundColor: "#6d351a",
+        justifyContent: "center",
+        alignItems: "center",
     },
     containerForm: {
         flex: 1,
-        backgroundColor: 'black',
+        backgroundColor: "black",
         borderTopLeftRadius: 25,
         borderTopRightRadius: 25,
-        paddingStart: '5%',
-        paddingEnd: '5%' 
+        paddingStart: "5%",
+        paddingEnd: "5%",
     },
-    title:{
+    title: {
         fontSize: 21,
-        fontWeight:'bold',
+        fontWeight: "bold",
         marginTop: 28,
         marginBottom: 12,
-        color: 'white'
+        color: "white",
     },
-    text:{
-        color: '#a1a1a1'
+    text: {
+        color: "#a1a1a1",
     },
     buttonContainer: {
-        flexDirection: 'row',
+        flexDirection: "row",
         justifyContent: "space-around",
-        marginTop: "20%"
+        marginTop: "20%",
     },
-    button:{
+    button: {
         backgroundColor: "#9A2E00",
         borderRadius: 20,
-        padding:15,
-        width: '45%', 
-        alignItems: 'center',
+        padding: 15,
+        width: "45%",
+        alignItems: "center",
     },
-    buttonText:{
-        color: 'white',
+    buttonText: {
+        color: "white",
         fontSize: 12,
-        fontWeight: 'bold'
+        fontWeight: "bold",
     },
-    buttonlogin:{
+    buttonlogin: {
         backgroundColor: "#9A2E00",
         borderRadius: 50,
-        width: '45%', 
-        alignItems: 'center',
-        justifyContent: 'center',
+        width: "45%",
+        alignItems: "center",
+        justifyContent: "center",
     },
-    login:{
-        color: 'white',
+    login: {
+        color: "white",
         fontSize: 12,
-        fontWeight: 'bold'
-    }
-    
+        fontWeight: "bold",
+    },
 });
